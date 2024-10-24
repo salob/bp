258ff88026e8cf44251646200938243c90465808
salob <sianob79@gmail.com>
2024-10-24 20:37:38 +0100

    additional styling and tidy up also add chart image

--- BPCalculatorTest/BloodPressureTest.cs
+++ BPCalculatorTest/BloodPressureTest.cs
@@ -6,10 +6,37 @@ using System.ComponentModel.DataAnnotations;
 public class BloodPressureTest
 {
   [DataTestMethod]
+
+  // Initial tests
   [DataRow(140, 90, BPCategory.High)]
   [DataRow(130, 55, BPCategory.PreHigh)]
   [DataRow(100, 79, BPCategory.Ideal)]
   [DataRow(89, 59, BPCategory.Low)]
+
+  // Low Blood Pressure (Systolic < 90, Diastolic < 60)
+  [DataRow(71, 40, BPCategory.Low)]
+  [DataRow(80, 50, BPCategory.Low)]
+  [DataRow(89, 59, BPCategory.Low)]
+
+  // Ideal Blood Pressure (90 ≤ Systolic < 120, 60 ≤ Diastolic < 80)
+  [DataRow(90, 60, BPCategory.Ideal)]
+  [DataRow(95, 65, BPCategory.Ideal)]
+  [DataRow(100, 70, BPCategory.Ideal)]
+  [DataRow(110, 75, BPCategory.Ideal)]
+  [DataRow(119, 79, BPCategory.Ideal)]
+
+  // Pre-High Blood Pressure (120 ≤ Systolic < 140, 60 ≤ Diastolic < 90)
+  [DataRow(120, 60, BPCategory.PreHigh)]
+  [DataRow(130, 70, BPCategory.PreHigh)]
+  [DataRow(135, 80, BPCategory.PreHigh)]
+  [DataRow(139, 89, BPCategory.PreHigh)]
+
+  // High Blood Pressure (Systolic ≥ 140, Diastolic ≥ 90)
+  [DataRow(140, 90, BPCategory.High)]
+  [DataRow(150, 95, BPCategory.High)]
+  [DataRow(160, 100, BPCategory.High)]
+  [DataRow(190, 100, BPCategory.High)]
+ 
   public void TestValidData(int sc, int ds, BPCategory cat)
   {
     BloodPressure bp = new BloodPressure() { Systolic = sc, Diastolic = ds };

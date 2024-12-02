934067889c9d767c5043e1631e1b2394cab63821
salob <sianob79@gmail.com>
2024-12-02 22:58:37 +0000

    update k6 scripts

--- PerformanceTests/k6-staging.js
+++ PerformanceTests/k6-staging.js
@@ -4,8 +4,8 @@ import http from "k6/http";
 export let options = {
 
   stages: [
-    { duration: '30s', target: 10 }, // Ramp up to 10 users over 30 seconds
-    { duration: '1m', target: 10 }, // Stay at 10 users for 1 minute
+    { duration: '30s', target: 100 }, // Ramp up to 100 users over 30 seconds
+    { duration: '1m', target: 100 }, // Stay at 100 users for 1 minute
     { duration: '30s', target: 0 },  // Ramp down to 0 users over 30 seconds
   ],
 

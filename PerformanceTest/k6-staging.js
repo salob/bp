8c9ed35d23c1027505970beb8a94f42a191d44d2
salob <sianob79@gmail.com>
2024-12-08 11:31:26 +0000

    https only

--- PerformanceTest/k6-staging.js
+++ PerformanceTest/k6-staging.js
@@ -27,8 +27,8 @@ function getRandomBloodPressure() {
 }
 // main k6 function
 export default function() {
-//const baseUrl = __ENV.WEB_APP_URI;
-const baseUrl = "https://sb-csd-bp-staging.azurewebsites.net/"
+const baseUrl = __ENV.WEB_APP_URI;
+//const baseUrl = "https://sb-csd-bp-staging.azurewebsites.net/"
 let res = http.get(baseUrl, {"responseType": "text"})
 
   check(res, {
